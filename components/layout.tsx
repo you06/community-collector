1e669bb60ec382a598dd2c07d6a199ba23f7d14c
you06 <you1474600@gmail.com>
2020-05-08 17:32:07 +0800

    init startup framework
    Signed-off-by: you06 <you1474600@gmail.com>

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,10 +3,16 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '[Your Name]'
+const name = 'you06'
 export const siteTitle = 'Next.js Sample Website'
 
-export default function Layout({ children, home }) {
+export default function Layout({
+  children,
+  home
+}: {
+  children: React.ReactNode
+  home?: boolean
+}) {
   return (
     <div className={styles.container}>
       <Head>
